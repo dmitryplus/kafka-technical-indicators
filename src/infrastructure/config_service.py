a3247bb269e82a56d6e7d51f448e07c0e8614c07
d.plushchenkov <d.plushchenkov@notamedia.ru>
2024-12-20 11:25:39 +0300

    update compose

---
+++ src/infrastructure/config_service.py
@@ -0,0 +1,59 @@
+import json
+import os
+
+from kafka import KafkaConsumer
+
+from kafka_service import KafkaService
+
+from .SingletonMeta import SingletonMeta
+
+class ConfigService(metaclass=SingletonMeta):
+    '''
+    Класс для работы с конфигурацией
+    '''
+
+    __configs: dict[str, str | dict] = None
+
+    __config_topic_name: str = None
+    __instrument_key: str = 'instruments'
+
+    @classmethod
+    def __init__(cls):
+
+        cls.__config_topic_name = os.environ.get('TOPIC_CONFIG', None)
+        if cls.get_config_topic_name() is None:
+            raise RuntimeError("TOPIC_CONFIG not find")
+
+        cls.init_config_from_kafka()
+
+    @classmethod
+    def get_config_topic_name(cls):
+        return cls.__config_topic_name
+
+    @classmethod
+    def get_instrument_key(cls):
+        return cls.__instrument_key
+
+    @classmethod
+    def get_config(cls):
+        return cls.__configs
+
+    @classmethod
+    def init_config_from_kafka(cls):
+
+        kafka_service = KafkaService()
+
+        consumer = KafkaConsumer(
+            kafka_service.get_config_topic_name(),
+            bootstrap_servers=[kafka_service.get_bootstrap()],
+            auto_offset_reset='earliest',
+            enable_auto_commit=False,
+            key_deserializer=lambda m: m.decode('utf-8'),
+            value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+            consumer_timeout_ms=3000
+        )
+
+        for message in consumer:
+            cls.__configs[message.key] = message.value
+
+
--- src/infrastructure/kafka_service.py
+++ src/infrastructure/kafka_service.py
@@ -6,15 +6,13 @@ from time import sleep
 
 from .SingletonMeta import SingletonMeta
 
+
 class KafkaService(metaclass=SingletonMeta):
     '''
     Класс для работы с kafka
     '''
 
     __bootstrap: str = None
-
-    __config_topic_name: str = None
-    __instrument_key: str = 'instruments'
 
     @classmethod
     def __init__(cls):
@@ -23,15 +21,10 @@ class KafkaService(metaclass=SingletonMeta):
         if cls.get_bootstrap() is None:
             raise RuntimeError("KAFKA CONNECT not find")
 
-        cls.__config_topic_name = os.environ.get('TOPIC_CONFIG', None)
-        if cls.get_config_topic_name() is None:
-            raise RuntimeError("TOPIC_CONFIG not find")
-
     @classmethod
     def wait_topic_exists(cls, topic: str):
 
         kafka_available = False
-
 
         while not kafka_available:
             try:
@@ -47,18 +40,9 @@ class KafkaService(metaclass=SingletonMeta):
                 print(e)
                 sleep(3)
 
-
     @classmethod
     def get_bootstrap(cls):
         return cls.__bootstrap
-
-    @classmethod
-    def get_config_topic_name(cls):
-        return cls.__config_topic_name
-    @classmethod
-    def get_instrument_key(cls):
-        return cls.__instrument_key
-
 
     @classmethod
     def send(cls, topic: str, key: str, value: str):
@@ -73,5 +57,3 @@ class KafkaService(metaclass=SingletonMeta):
 
         producer.send(topic, key=key, value=value)
         producer.close()
-
-
