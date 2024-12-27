225a9576771f47771578c7ffebd9c850c6d37397
d.plushchenkov <d.plushchenkov@notamedia.ru>
2024-12-27 14:31:45 +0300

    update macd

--- images/macd/src/main.py
+++ images/macd/src/main.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 
 from infrastructure.kafka_service import KafkaService
 from infrastructure.config_service import ConfigService
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 
 from macd import Macd
 from params import Params
@@ -39,14 +39,14 @@ def main():
 
     kafka_service.wait_topic_exists(topic)
 
-    #собираем данные из истории
+    # собираем данные из истории
     consumer_history = KafkaConsumer(
         topic,
         bootstrap_servers=[(KafkaService()).get_bootstrap()],
         auto_offset_reset='earliest',
         key_deserializer=lambda m: m.decode('utf-8'),
         value_deserializer=lambda m: json.loads(m.decode('utf-8')),
-        consumer_timeout_ms=5000
+        consumer_timeout_ms=1000
     )
 
     for message in consumer_history:
@@ -62,19 +62,41 @@ def main():
 
         figies[message.key][message.value['time']] = message.value['close']
 
-        print(message.key, message.value['time'], message)
-
     consumer_history.close()
 
-    #вычисляем по последнем данным из истории
+    # вычисляем по последнем данным из истории
     for figi in figies:
         if len(figies[figi]) > Params().get_candles_count():
-            macd_last_value = convert_macd_value(Macd(figi, figies[figi]).get_last_value())
-            if len(macd_last_value) > 0:
-                kafka_service.send(exit_topic, figi, macd_last_value)
-            print(figi, macd_last_value)
 
-    #дальше реалтайм поток
+            producer = KafkaProducer(
+                bootstrap_servers=kafka_service.get_bootstrap(),
+                key_serializer=str.encode,
+                value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+            )
+
+            times = sorted(list(figies[figi].keys()), key=lambda x: x.lower())
+
+            for i in range(Params().get_candles_count(), len(figies[figi])):
+
+                start_key = i - Params().get_candles_count()
+                stop_key = i + 1
+
+                part_times = times[start_key:stop_key]
+
+                part_figies = {}
+
+                for time_key in part_times:
+                    part_figies[time_key] = figies[figi][time_key]
+
+                macd_last_value = convert_macd_value(Macd(figi, part_figies).get_last_value())
+                if len(macd_last_value) > 0:
+                    producer.send(exit_topic, key=figi, value=macd_last_value)
+
+                print(figi, macd_last_value)
+
+            producer.close()
+
+    # дальше реалтайм поток
     consumer = KafkaConsumer(
         topic,
         bootstrap_servers=[(KafkaService()).get_bootstrap()],
